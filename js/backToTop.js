d431c2e32841d5f838fb502e0abe1091160cd225
Arthur C Edwards <Arthur.Edwards321@gmail.com>
2020-01-12 13:59:51 -0500

    reveal js works. arrow shows up on scroll

--- js/backToTop.js
+++ js/backToTop.js
@@ -14,18 +14,18 @@
 
 // When the user clicks on the button, scroll to the top of the document
 
-var mybutton = document.getElementById("myBtn");
+// var mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () { scrollFunction() };
+// // When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = function () { scrollFunction() };
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
-}
+// function scrollFunction() {
+//     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+//         mybutton.style.display = "block";
+//     } else {
+//         mybutton.style.display = "none";
+//     }
+// }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
@@ -34,11 +34,11 @@ function topFunction() {
 }
 
 function scrollToTop() {
-    var position =
-        document.body.scrollTop || document.documentElement.scrollTop;
+    var position = document.body.scrollTop || document.documentElement.scrollTop;
     if (position) {
-        window.scrollBy(0, -Math.max(1, Math.floor(position / 10)));
-        scrollAnimation = setTimeout("scrollToTop()", 30);
-    } else clearTimeout(scrollAnimation);
+        window.scrollBy(0, -Math.max(10, Math.floor(position / 10)));
+        scrollAnimation = setTimeout('scrollToTop()', 10);
+    }
+    else clearTimeout(scrollAnimation);
 }
 
--- js/revealArrow.js
+++ js/revealArrow.js
@@ -2,8 +2,9 @@
 //Get the button
 var mybutton = document.getElementById("arrowUpBtn");
 
-
 // When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         mybutton.style.display = "block";
